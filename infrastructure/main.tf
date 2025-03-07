5872e8efd5041214b007d36d0ff6317e8f894d89
shivareddysama <samashivareddy9002@gmail.com>
2025-03-06 20:42:32 -0500

    Fixed formatting and added Terraform script

---
+++ infrastructure/main.tf
@@ -0,0 +1,20 @@
+# Azure Provider Configuration
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "shiva" {
+  name     = "shiva-resources"
+  location = "Canada Central"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "shiva" {
+  name                     = "shivastorageacc123"
+  resource_group_name      = azurerm_resource_group.shiva.name
+  location                 = azurerm_resource_group.shiva.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}resource "azurerm_resource_group" "another" {name="test"location="West US"}
+resource "azurerm_resource_group" "another" {name="test"location="West US"}
